559fcc032fe75eae910d4e88df5629e81dd7c8a6
mohamed aziz ammar <azizammar67@gmail.com>
2022-08-21 15:09:25 +0100

    update

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,53 +1,48 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import abb from './img/abb.png';
 const Head = () => {
+
   return (
-    <div className='nav'>
-        <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div>
+      <header>
+        <nav className='haha'>
+          <div>
+            <img className='cb' src={abb} alt="" />
+          </div>
+          <div>
+            <input className='rbg' type="text" />
+            <button>Search</button>
+          </div>
+          <div>
+            <ul>
+            <li id="li3" class="navbarlist">Services
+                <ul class="list2">
+                  <a href="/quran/Quran.html">
+                  <li class="second-list">Quran</li>
+                  </a>
+                  <a href="/biography of the prophet/botp.html">
+                  <li class="second-list">Biography of the Prophet</li>
+                  </a>
+                  <a href="/ablution/ablution.html">
+                  <li class="second-list">Ablution</li>
+                  </a>
+                  <a href="/prayer/prayer.html">
+                  <li class="second-list">Prayer</li>
+                  </a>
+                  <a href="/al haj/al_haj.html">
+                  <li class="second-list">Al Haj</li>
+                  </a>
+                </ul>  
+                <img src="" alt="" />
+                <h3 className='aqw'>0</h3>
+            </li>
+            </ul>
+                
+          </div>
+        </nav>
+      </header>
     </div>
+    
   )
 }
 
